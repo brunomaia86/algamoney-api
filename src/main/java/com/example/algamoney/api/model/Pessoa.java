9e94def138569c07d166665f75c58bc70ef841b1
bruno <bruno@DESKTOP-61466HC>
2018-03-06 23:23:01 -0300

    criacao entidade

---
+++ src/main/java/com/example/algamoney/api/model/Pessoa.java
@@ -0,0 +1,33 @@
+package com.example.algamoney.api.model;
+
+public class Pessoa {
+
+	private String nome;
+	private Endereco endereco;
+	private boolean ativo;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+
+}
