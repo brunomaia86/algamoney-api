b8351b0471bd62673c89f289302cc9cb5d99e8bc
bruno <bruno@DESKTOP-61466HC>
2018-03-07 23:39:17 -0300

    add metodo buscar pessoa por codigo

--- src/main/java/com/example/algamoney/api/resource/PessoaResource.java
+++ src/main/java/com/example/algamoney/api/resource/PessoaResource.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,6 +41,10 @@ public class PessoaResource {
 		return ResponseEntity.created(uri).body(pessoaSalva);
 	}
 	
-	
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Pessoa> buscarPorCodigo(@PathVariable Long codigo) {
+		Pessoa pessoa = pessoaRepository.findOne(codigo);
+		return pessoa == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(pessoa);
+	}
 
 }
